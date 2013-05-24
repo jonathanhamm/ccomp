5104d901457cf8da032035beb27a69f2416902b9
Jon <jonathan-hamm@utulsa.edu>
2013-05-24 16:19:58 -0500

    lex.c
    Just an update

--- lex.h
+++ lex.h
@@ -24,6 +24,8 @@ typedef struct lex_s lex_s;
 typedef struct idtable_s idtable_s;
 typedef struct idtnode_s idtnode_s;
 typedef struct token_s token_s;
+typedef struct machnode_s machnode_s;
+typedef struct mach_s mach_s;
 
 struct token_s
 {
@@ -51,14 +53,39 @@ struct idtable_s
     idtnode_s *root;
 };
 
+struct machnode_s
+{
+    uint16_t index;
+    uint16_t jump;
+    uint16_t nbranches;
+    token_s *token;
+    machnode_s **branches;
+};
+
+struct mach_s
+{
+    token_s *nterm;
+    uint16_t nstates;
+    machnode_s *curr;
+    machnode_s *start;
+    mach_s *next;
+};
+
 struct lex_s
 {
+    uint16_t nmachs;
+    mach_s *curr;
+    mach_s *machs;
     idtable_s *kwtable;
 };
 
+extern u_char *readfile (const char *file);
+extern token_s *lex (lex_s *lex, u_char *buf);
 extern lex_s *lex_s_ (void);
 extern token_s *buildlex (const char *file);
 extern idtable_s *idtable_s_ (void);
+extern void addstate (mach_s *mach, token_s *tok);
+extern void addmachine (lex_s *lex, token_s *tok);
 extern void idtable_insert (idtable_s *table, u_char *str);
 extern int idtable_lookup (idtable_s *table, u_char *str);
 extern int addtok (token_s **tlist, u_char *lexeme, uint32_t lineno, uint16_t type, uint16_t attribute);
