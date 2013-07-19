7f5b3bebb7308ed74627ba872f74624cd821a2bd
Jon <jonathan-hamm@utulsa.edu>
2013-07-19 16:25:32 -0500

    Edited main to allow flexibility in files being opened

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include "lex.h"
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
     token_s *tokens;
-        
-    tokens = lex (buildlex ("regex_pascal"), readfile ("samples/lex_sample1"));
     
+    printf("%s\n%s\n", argv[1], argv[2]);
+    if (argc == 1)
+        tokens = lex (buildlex ("regex_pascal"), readfile ("samples/lex_sample1"));
+    else if (argc == 2)
+        tokens = lex (buildlex ("regex_pascal"), readfile (argv[1]));
+    else if (argc == 3)
+        tokens = lex (buildlex (argv[2]), readfile (argv[1]));
+    else
+        perror("Argument Error");
     return 0;
 }
 
