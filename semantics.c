851e389067d40187414cb771d2626048761c4b99
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2013-09-09 00:44:12 -0500

    Wrote 'complete' code for semantic decoratons parser, but it is not tested or debugged.

--- semantics.c
+++ semantics.c
@@ -15,23 +15,6 @@
 #define REGEX_DECORATIONS_FILE "regex_decorations"
 #define MACHID_START            5000
 
-/*
- if
- else
- then
- and
- or
- \(
- \)
- .
- ,
- ;
- [
- ]
- 
- */
-
-
 enum semantic_types_ {
     SEMTYPE_IF = LEXTYPE_START,
     SEMTYPE_THEN,
@@ -47,74 +30,17 @@ enum semantic_types_ {
     SEMTYPE_SEMICOLON,
     SEMTYPE_OPENBRACKET,
     SEMTYPE_CLOSEBRACKET,
+    SEMTYPE_ID,
     /*gap for id types*/
-    SEMTYPE_NONTERM = 5000,
+    SEMTYPE_NONTERM,
     SEMTYPE_NUM,
     SEMTYPE_RELOP,
     SEMTYPE_ASSIGNOP,
     SEMTYPE_CROSS,
     SEMTYPE_ADDOP,
-    SEMTYPE_MULOP
+    SEMTYPE_MULOP,
+    SEMTYPE_EOF,
 };
-
-/*
-<start>->
-<statements>
-
-<statements>->
-<statement>
-<statements>
-|
-ε
-
-<statement>->
-nonterm . id assignop <expression>
-|
-if <expression> then <statements> <else>
-<else>->
-else <statements> fi
-|
-fi
-
-<expression> ->
-<simple_expression> <expression'>
-<expression'> ->
-relop <simple_expression>
-|
-ε
-
-<simple_expression> ->
-<sign> <term> <simple_expression'>
-|
-<term> <simple_expression'>
-
-<simple_expression'> ->
-addop <term> <simple_expression'>
-|
-ε
-
-<term> ->
-<factor> <term'>
-
-<term'> ->
-mulop <factor> <term'>
-|
-ε
-
-<factor> ->
-id <factor'>
-|
-num
-|
-not <factor>
-
-<factor'> ->
-[ <expression> ]
-|
-ε
-
-<sign> ->
-\+ | \-*/
 
 static void sem_start (token_s **curr);
 static void sem_statements (token_s **curr);
@@ -129,7 +55,7 @@ static void sem_term_ (token_s **curr);
 static void sem_factor (token_s **curr);
 static void sem_factor_ (token_s **curr);
 static void sem_sign (token_s **curr);
-
+static void sem_match (token_s **curr, int type);
 
 lex_s *semant_init(void)
 {
@@ -166,67 +92,231 @@ void sem_statements (token_s **curr)
 {
     switch((*curr)->type.val) {
         case SEMTYPE_IF:
+        case SEMTYPE_NONTERM:
             sem_statement(curr);
             sem_statements(curr);
         case SEMTYPE_FI:
         case SEMTYPE_ELSE:
+        case SEMTYPE_EOF:
             break;
-            
+        default:
+            printf("Syntax Error at line %d: Expected if nonterm fi else or $ but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
     }
 
 }
 
 void sem_statement (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_NONTERM:
+            *curr = (*curr)->next;
+            sem_match(curr, SEMTYPE_DOT);
+            sem_match(curr, SEMTYPE_ID);
+            sem_match(curr, SEMTYPE_ASSIGNOP);
+            sem_expression(curr);
+            break;
+        case SEMTYPE_IF:
+            *curr = (*curr)->next;
+            sem_expression(curr);
+            sem_match(curr, SEMTYPE_THEN);
+            sem_statements(curr);
+            sem_else(curr);
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected nonterm or if but got %s", (*curr)->lineno, (*curr)->lexeme);
+            break;
+    }
 }
 
 void sem_else (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_ELSE:
+            *curr = (*curr)->next;
+            sem_expression(curr);
+            break;
+        case SEMTYPE_FI:
+            *curr = (*curr)->next;
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected else or fi but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            break;
+    }
 }
 
 void sem_expression (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_ADDOP:
+        case SEMTYPE_NOT:
+        case SEMTYPE_NUM:
+        case SEMTYPE_ID:
+            sem_simple_expression(curr);
+            sem_expression_(curr);
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected addop not num or id but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            break;
+    }
 }
 
 void sem_expression_ (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_RELOP:
+            *curr = (*curr)->next;
+            sem_simple_expression(curr);
+            break;
+        case SEMTYPE_CLOSEBRACKET:
+        case SEMTYPE_FI:
+        case SEMTYPE_ELSE:
+        case SEMTYPE_THEN:
+        case SEMTYPE_IF:
+        case SEMTYPE_NONTERM:
+        case SEMTYPE_EOF:
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected relop ] fi else then if nonterm or $ but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+            
+    }
 }
 
 void sem_simple_expression (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_ADDOP:
+            *curr = (*curr)->next;
+            sem_simple_expression(curr);
+            break;
+        case SEMTYPE_NOT:
+        case SEMTYPE_NUM:
+        case SEMTYPE_ID:
+            sem_term(curr);
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected + - not number or identifier but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void sem_simple_expression_ (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_ADDOP:
+            *curr = (*curr)->next;
+            sem_term(curr);
+            sem_simple_expression_(curr);
+            break;
+        case SEMTYPE_CLOSEBRACKET:
+        case SEMTYPE_RELOP:
+        case SEMTYPE_FI:
+        case SEMTYPE_ELSE:
+        case SEMTYPE_THEN:
+        case SEMTYPE_IF:
+        case SEMTYPE_NONTERM:
+        case SEMTYPE_EOF:
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected + - ] = < > <> <= >= fi else then if nonterm or $ but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void sem_term (token_s **curr)
 {
-    
+    sem_factor(curr);
+    sem_term_(curr);
 }
 
 void sem_term_ (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_MULOP:
+            *curr = (*curr)->next;
+            sem_factor(curr);
+            sem_term_(curr);
+            break;
+        case SEMTYPE_CLOSEBRACKET:
+        case SEMTYPE_ADDOP:
+        case SEMTYPE_RELOP:
+        case SEMTYPE_FI:
+        case SEMTYPE_ELSE:
+        case SEMTYPE_THEN:
+        case SEMTYPE_IF:
+        case SEMTYPE_NONTERM:
+        case SEMTYPE_EOF:
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected * / ] + - = < > <> <= >= fi else then if nonterm or $ but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void sem_factor (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_ID:
+            *curr = (*curr)->next;
+            sem_factor_(curr);
+            break;
+        case SEMTYPE_NUM:
+            *curr = (*curr)->next;
+            break;
+        case SEMTYPE_NOT:
+            *curr = (*curr)->next;
+            sem_factor(curr);
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected identifier number or not but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void sem_factor_ (token_s **curr)
 {
-    
+    switch((*curr)->type.val) {
+        case SEMTYPE_OPENBRACKET:
+            *curr = (*curr)->next;
+            sem_expression(curr);
+            sem_match(curr, SEMTYPE_CLOSEBRACKET);
+            break;
+        case SEMTYPE_CLOSEBRACKET:
+        case SEMTYPE_MULOP:
+        case SEMTYPE_ADDOP:
+        case SEMTYPE_RELOP:
+        case SEMTYPE_FI:
+        case SEMTYPE_ELSE:
+        case SEMTYPE_THEN:
+        case SEMTYPE_IF:
+        case SEMTYPE_NONTERM:
+        case SEMTYPE_EOF:
+            break;
+        default:
+            printf("Syntax Error at line %d: Expected [ ] * / + - = < > <> <= >= fi else then if nonterm or $ but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+            exit(EXIT_FAILURE);
+    }
 }
 
 void sem_sign (token_s **curr)
 {
-    
+    if ((*curr)->type.val == SEMTYPE_ADDOP) {
+        *curr = (*curr)->next;
+        
+    }
+    else {
+        printf("Syntax Error at line %d: Expected + or - but got %s\n", (*curr)->lineno, (*curr)->lexeme);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sem_match (token_s **curr, int type)
+{
+    if ((*curr)->type.val == type)
+        *curr = (*curr)->next;
+    else {
+        printf("Syntax Error at line %d: Got %s\n", (*curr)->lineno, (*curr)->lexeme);
+        exit(EXIT_FAILURE);
+    }
 }
