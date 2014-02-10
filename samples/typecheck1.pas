df2eb1b7d98f3612d8439017628f7e4d64c561b6
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2014-02-10 15:04:27 -0600

    Added another sample file typecheck1.pas

---
+++ samples/typecheck1.pas
@@ -0,0 +1,30 @@
+program bob (input, output);
+var a : integer; var c : real;
+procedure lolwut (a : integer; b : real; c : real; kimjongil : array[14..1000] of real; bosstweed : array[1..1000] of type real);
+    begin
+        if a <> b then
+        call lolwut(a, b, c);
+            call lolwut(a, c, b)
+        ;
+kimjongil[a*a+a] := 333*34;
+b := c*b*a*c/3;
+kimjongil := bosstweed;
+call lolwut;
+        c := kimjongil[a*b]
+    end;
+
+
+begin
+a := 3;
+    if (a*32 mod 4) <> c then
+        call lolwut(234, 32, 23, 23, 23)
+    else
+        begin
+            while a < c do
+                begin
+                    a := a*32*c;
+                    c := a*23*23/4*32/23
+                end
+end;
+        c := a[4]
+end.
