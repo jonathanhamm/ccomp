96a9ade247bfe674e2ee0941a48d424ff845377a
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2014-02-11 02:00:38 -0600

    Added new test files.

---
+++ samples/simple.pas
@@ -0,0 +1,23 @@
+program bob (input, output);
+var a : integer;
+var b : array[1..10] of real;
+var c : array[1..10] of integer;
+var d : array[1..11] of integer;
+var e : real;
+var f : array[1..10] of real;
+begin
+    c := d;
+    b := f;
+    while a + b[a] <= 10 do
+    begin
+        a := a + b[a];
+        if a = e then
+            begin
+                e := c[32];
+                c[31] := e
+            end
+        else
+            e := c[31];
+    d[10] := c[d[c[3 mod 4]]]
+    end
+end.
---
+++ samples/typecheck1_working.pas
@@ -0,0 +1,44 @@
+program bob (input, output);
+var a : integer; var c : real; var d : array[2332..3232] of real;
+var torquemada : array[32..2332] of integer; var putin : array[333..32322] of real;
+var kimjongun : array[13..1000] of real;
+var kimjongil : array[14..1000] of real;
+var bosstweed : array[1..1000] of real;
+procedure lolwut (a : integer; b : real; c : real; kimjongil : array[14..1000] of real;
+bosstweed : array[1..1000] of real);
+    begin
+        if a <> b then
+            a := a * b
+        ;
+        kimjongil[a*a+a] := 333*34;
+        b := c*b*a*c/3;
+        kimjongil := bosstweed;
+        c := kimjongil[a*b]
+    end;
+begin
+    a := 3;
+    if (a*32 mod 4) <> c then
+        call lolwut(1, 1.1, 3.3, kimjongil, bosstweed)
+    else
+        begin
+            while a < c do
+                begin
+                    a := a*32*c;
+                    c := a*23*23/4*32/23
+                end
+end;
+        if not (d[3] = putin[11+32+3]) and (torquemada[323] <> 432) or (a+c/c+32 <= putin[32]) then
+            begin
+                d[2] := 32
+            end
+        else
+            begin
+                putin[88*88*a] := 23.132+32/32+342+(3 mod 44);
+                while (a * d[2222]+3 <> 32) and (torquemada[2332+32+(8 mod 4)] <> putin[2323])
+                    or (putin[32] >= 2332) or (torquemada[3223+3] < 2323) do
+                    begin
+                        putin[3232] := 2332+a
+                    end;
+                    call lolwut(1, 1.1, 3.3, kimjongil, bosstweed)
+            end
+end.
