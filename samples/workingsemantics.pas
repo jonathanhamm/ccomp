a52b3c0c3146da8db7e2de9aeb7bef5e90a499bc
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2014-02-10 22:25:19 -0600

    Created elaborate example without semantics errors.

--- samples/workingsemantics.pas
+++ samples/workingsemantics.pas
@@ -1,7 +1,9 @@
 program bob (input, output);
 var tom : integer; var ted : real;
 var omg : array[23..44] of real;
-procedure omg (a : integer; b : real; c : real);
+var WAHAHA : array[234444..4444444] of integer;
+var lolol : real;
+procedure omgAliens (a : integer; b : real; c : real);
 var derp : real;
 begin
     derp := 10
@@ -10,7 +12,7 @@ end;
 procedure bob (a : integer; b : real; c : real);
 	var zomg : integer; var herp : array[1..2321] of real;
 	procedure lolololol (d:real);
-	var derp : real;
+    var derp : real; var zomb : integer;
 	begin
         zomg := 32;
 		if 43 > herp[32*32/44.2] then
@@ -18,62 +20,63 @@ procedure bob (a : integer; b : real; c : real);
 		else
 			derp := 4.7E3 + a+22.1 + (33-4)
 		;
-        a := zomb + c;
+        a := zomb + a;
 		derp := -3*33;
         c := 3 * 1111.1;
         lolol := -3.23344;
-		zomg := +32.2 + 3;
+		zomg := +32 + 3;
+        call omgAliens(1*4, 32.32E3, 23.23);
 		omg[32] := -omg[32];
 		herp[22*23232.3] := +omg[23];
 		while d <> ted do
 		begin
 			ted := d-1;
-			call lolololol(d * ted mod 322 div 3232.2);
+			call lolololol((tom * a mod 322) div 3232.2);
 			if d = 3 then
 			begin 
 				ted := tom div d;
-                zomg := 33333.1;
+                zomg := 33333;
 				tom := tom * d
 			end
 		end;
-		derp := derp mod c + b/a - ted*omg[30]
+		derp := tom mod a + b/a - ted*omg[30]
 	end;
 begin
-	while zomg <> omg do
-		zomg := omg*zomg and ted-omg[3 mod 4];
-	if omg <= 823.22 then
-		if zomg >= omg then
-			if omg[33] = omg then
+	while zomg <> b do
+		zomg := tom*zomg and tom-WAHAHA[3 mod 4];
+	if b <= 823.22 then
+		if zomg >= b then
+			if omg[33] = omg[32] then
 				call bob (3,33.1,33.1)
 			else
-				call bob (omg,3.1,888.1)
+				call bob (WAHAHA[2*32],3.1,888.1)
 		else
-			call bob (ted, 32, 2)
+			call bob (a, 32, 2)
 	else
 	begin
 		if omg[24] = 39 then
 		begin
-			call bob(ted*323 div 32 mod 3343, 32.1+omg, 32.1);
+			call bob(100, 32.1+omg[33*omg[3*a]], ted*323 div (32 mod 3343));
 			if omg[24*ted] < 5 then
 				omg[24 div 10] := 38
 			else
 			begin
 				omg[24 div ted] := 232332;
 				ted := 3223.2323;
-				if omg[24 and 3.3 or 33.23] > 2323.2 then
+				if omg[24 and 3 or 33] > 2323.2 then
 					call bob (3,33.2,33.1)
 			end
 		end;
-		if not omg[24 div 10] <> 32.2 then
-			call bob(ted*323 div 32 mod 3343, 32.1+omg, 32.1)
+		if not WAHAHA[24 div 10] <> 32.2 then
+			call bob(WAHAHA[8 mod a]*323 or 32 mod 3343, 32.1+omg, 32.1)
 		else
 		begin
-			bob := bob and omg or 32.1 + 23;
-			while not 32.444 > omg do
+			omg[32] := a and WAHAHA[323*WAHAHA[32]] or (32 + 23);
+			while not (+(-(32+444))) > omg[33] do
 				omg := +3232.1 * omg
 			;
-			if not 32.2 then
-				call bob (not 2323.2, -34 < omg[32.1], +23+3 <> omg, 5+1)
+			if not (-32+32) then
+				call bob (not 2323, -34 < omg[32.1], +23+3 <> omg[32])
 			else if 32.1 then
 				call bob (3,33.2,33.1)
 		end
@@ -86,19 +89,20 @@ procedure lolwut (a : integer; b : real; c : real);
 	var a : real; var putin : integer;
 	begin
 		if not putin then
-			call lolwut(2323, 234.2, 233,1)
+			call lolwut(2323, 234.2*32/32 div 32, 233)
 		else
 			call lawl
 	end;
 begin
 	while not ( 3*2/(x <> y)) do
-		x := y*x
+		x := x*x*32*WAHAHA[23*32*WAHAHA[23]]
 end;
 
 procedure bosstweed (aaaaaaa : real ; omnomnom : 
 
 integer; omgomgomg : real);
 var hahah : array[12..3124] of real;
+var aaa : integer;
 begin
 	if not aaa*omnomnom*omgomgomg*(aaa+(omnomnom*(-aaaaaaa+3.3)*32)
 	 mod (33+23*4 or 3))/32.1 <> 32.232E-3 then
@@ -107,7 +111,7 @@ begin
 		call bosstweed (34.2 , omnomnom, 234*2.2)
 end;
 begin
-	tom := ted+32+31+tom*5*(5 + (33443+22+ted) * ((53+22)*888)+43.2)+44323.32134E-32;
+	omg[32+3] := ted+32+31+tom*5*(5 + (33443+22+ted) * ((53+22)*888)+43.2)+44323.32134E-32;
 	call bob (+3, -324.1, ted * 23);
 	ted := -32.443E-12
 end.
